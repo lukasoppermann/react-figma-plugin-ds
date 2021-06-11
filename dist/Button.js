01e65d140b7c168fef91e112b5e5135c97c4217d
Lukas Oppermann <oppermann.lukas@gmail.com>
2021-06-11 21:01:23 +0200

    build button autofocus

--- dist/Button.js
+++ dist/Button.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
 var Button = function (_a) {
-    var children = _a.children, className = _a.className, isSecondary = _a.isSecondary, isTertiary = _a.isTertiary, isDisabled = _a.isDisabled, onClick = _a.onClick, isDestructive = _a.isDestructive, type = _a.type;
+    var children = _a.children, className = _a.className, isSecondary = _a.isSecondary, isTertiary = _a.isTertiary, isDisabled = _a.isDisabled, onClick = _a.onClick, isDestructive = _a.isDestructive, type = _a.type, autofocus = _a.autofocus;
     className = className || "";
     type = type || "submit";
     var level = isTertiary ? "tertiary" : isSecondary ? "secondary" : "primary";
@@ -13,7 +13,7 @@ var Button = function (_a) {
     if (isSecondary && isTertiary) {
         console.warn("Use only one level modificator \"isSecondary\" or \"isTertiary\"");
     }
-    return (react_1.default.createElement("button", { onClick: onClick, className: "button button--" + level + modificator + " " + className, disabled: isDisabled, type: type }, children));
+    return (react_1.default.createElement("button", { autoFocus: autofocus, onClick: onClick, className: "button button--" + level + modificator + " " + className, disabled: isDisabled, type: type }, children));
 };
 exports.default = Button;
 //# sourceMappingURL=Button.js.map
